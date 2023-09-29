074c05729edf47f0fb2497bd1c3299227006ee95
WillianFarias <williansousafarias@gmail.com>
2023-09-28 21:56:40 -0300

    Entidade Setor

--- src/main/java/com/sea/desafio/entidades/Setor.java
+++ src/main/java/com/sea/desafio/entidades/Setor.java
@@ -1,6 +1,7 @@
 package com.sea.desafio.entidades;
 
 import java.util.List;
+import java.util.Objects;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -22,4 +23,44 @@ public class Setor {
 	@OneToMany(mappedBy = "setor")
 	private List<Cargo> cargos;
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public List<Cargo> getCargos() {
+		return cargos;
+	}
+
+	public void setCargos(List<Cargo> cargos) {
+		this.cargos = cargos;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Setor other = (Setor) obj;
+		return Objects.equals(id, other.id);
+	}
 }
